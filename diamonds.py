e0edc66ca483ba8542d55c69ef6b946124f21309
LaNoir <goflvhxj159@naver.com>
2018-02-01 16:54:21 +0900

    HW_1 diamonds

---
+++ diamonds.py
@@ -0,0 +1,15 @@
+
+
+
+i = int(input("길이 :"))
+
+#if i%2 == 0:
+
+
+
+for x in range(1,line*2,2):
+    print(" "*((line*2-1-x)//2))+("*"*x)
+
+
+for y in range(line * 2-3, 0, -2):
+    print((" " * ( (line * 2 - 1 - y) // 2 )) + "*" * y)
